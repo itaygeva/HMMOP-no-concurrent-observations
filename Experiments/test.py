501c98ff0015c5a64712a4b2b98dd8045c5aef7a
Liad Ben Shachar <liad.mbs@gmail.com>
2023-09-15 12:15:51 +0300

    New version of pomegranate. Using DenseHMM. This is not working due to different sizes of arrays

--- Experiments/test.py
+++ Experiments/test.py
@@ -1,14 +1,18 @@
 from Data.Readers.brown_corpus_reader import BCReader
 import Omission.utils as omitter
-from Models.hmmlearn_wrapper import hmmlearn_wrapper
+from Models.pome_wrapper import pome_wrapper
 from Evaluations import utils as evaluations
 import numpy as np
+import time
 
 # test variables - temp
 n_states = 11
 n_iter = 20
 omission_prob = 0.7
 
+
+# Record the start time
+start_time = time.time()
 
 # import and prepare data
 brown_reader = BCReader()
@@ -17,18 +21,32 @@ brown_emission_prob = brown_reader.get_emission_prob()
 omitted_brown_data, omitted_brown_data_idx = omitter.bernoulli_experiments(omission_prob,brown_data)
 
 # fit models and get transition matrices
-hmmlearn_model = hmmlearn_wrapper(n_states, n_iter,brown_emission_prob)
+pome_model = pome_wrapper(n_states, n_iter, brown_emission_prob, freeze_distributions=True)
 
+pome_model.fit(brown_data)
+hmmlearn_transmat = pome_model.transmat
 
+"""
 hmmlearn_model.fit(omitted_brown_data)
 hmmlearn_omitted_transmat = hmmlearn_model.transmat
 
 
-hmmlearn_model.fit(brown_data)
-hmmlearn_transmat = hmmlearn_model.transmat
 
 
 
 # run evaluations
 evaluations.compare_mat_l1(hmmlearn_omitted_transmat,hmmlearn_transmat)
+"""
 
+
+
+
+
+# Record the end time
+end_time = time.time()
+
+# Calculate the elapsed time
+elapsed_time = end_time - start_time
+
+# Print the elapsed time in seconds
+print(f"Program executed in {elapsed_time:.2f} seconds")
--- Models/pome_wrapper.py
+++ Models/pome_wrapper.py
@@ -1,35 +1,105 @@
-from pomegranate import *
+import os
+
+import pomegranate.hmm as hmm
+import pomegranate.distributions as distributions
 import random
 import numpy as np
 from Models.model_wrapper import model_wrapper
+import pickle
+import inspect
+import torch
 
 
 class pome_wrapper(model_wrapper):
-    def __init__(self, n_components, n_iter, emission_prob):
+    def __init__(self, n_components, n_iter, emission_prob, freeze_distributions=False):
         super().__init__(n_components, n_iter)
-        self.full_states = None
-        self.states = None
+        self._model = None
         self.emission_prob = emission_prob
+        self.freeze_distributions = freeze_distributions
 
+        print("creating model")
         # create model
+        self.generate_initial_model()
 
-        self._model = HiddenMarkovModel()
-        self.add_states_with_dist()
-        self.add_random_transitions()
-        self._model.bake()
+        data_dir = os.path.dirname(inspect.getfile(model_wrapper))
+        self.cache_dir = os.path.join(data_dir, 'Cache')
 
+    def generate_initial_model(self):
+        dists = [distributions.Categorical(np.atleast_2d(dist), frozen=self.freeze_distributions) for dist in self.emission_prob]
+        edges = np.random.rand(self.n_components, self.n_components)
+        starts = np.random.rand(self.n_components)
+        starts = starts/np.sum(starts)
+        ends = np.random.rand(self.n_components)
+        self._model = hmm.DenseHMM(dists, edges=edges, starts=starts, ends=ends, max_iter=1)
+
+    def fit(self, data):
+        # need to add the tags in here
+        print("running fit")
+        cache_filename = os.path.join(self.cache_dir, 'model.pkl')
+        # Check if the pickle file exists
+        if os.path.isfile(cache_filename):
+            # If the file exists, load the variable from the file
+            with open(cache_filename, 'rb') as file:
+                self._model = pickle.load(file)
+
+        else:
+            # data = self.push_data_to_gpu(data)
+            self._model = hmm.DenseHMM()
+            self._model.fit(X=self.convert_data(data))
+            with open(cache_filename, 'wb') as file:
+                pickle.dump(self._model, file)
+
+    @property
+    def transmat(self):
+        try:
+            return self._model.edges
+        except AttributeError as e:
+            print(f"Model not initialized with fit, exception was raised: {e}")
+
+    @property
+    def startprob(self):
+        try:
+            return self._model.starts
+        except AttributeError as e:
+            print(f"Model not initialized with fit, exception was raised: {e}")
+
+    @property
+    def emissionprob(self):
+        return self.create_emissionprob()
+
+    def create_emissionprob(self):
+        self._model = hmm.DenseHMM()
+        emission_prob = []
+        for prob in self._model.distributions:
+            emission_prob.append(np.array(prob.probs))
+
+        return np.vstack(emission_prob)
+
+    def push_data_to_gpu(self, data):
+        tensors = [torch.tensor(sentence).float().cuda() for sentence in data]
+        return tensors
+
+    def convert_data(self, data):
+        data = [torch.from_numpy(arr).reshape(-1,1) for arr in data]
+        print(data[0])
+        return data
+
+
+
+
+
+"""
     def add_states_with_dist(self):
         dist_dict = self.create_dist_dict()
         states = []
         # creating states from dist and dict
-        for state_dist in dist_dict:
+        for i, state_dist in enumerate(dist_dict):
             dist = DiscreteDistribution(state_dist)
-            states.append(State(dist))
+            states.append(State(dist, name=str(i)))
         # adding states
         self.states = states
         self.full_states = self.states + [self._model.start, self._model.end]
         self._model.add_states(self.states)
-
 
     def add_start_and_end_transitions(self):
         for state in self.states:
@@ -44,11 +114,9 @@ class pome_wrapper(model_wrapper):
             for state_b in self.states:
                 self._model.add_transition(state_a, state_b, random.uniform(0, 1))
 
-    def fit(self, data):
-        # need to add the tags in here
-        print("running fit")
-        self._model.freeze_distributions()
-        self._model.fit(sequences=data, max_iterations=1)
+
+
+
 
     def create_dist_dict(self):
         # Should create a dict of dist from the self.emission_prob, as seen in the example from ChatGPT:
@@ -59,32 +127,4 @@ class pome_wrapper(model_wrapper):
         for state_prob in self.emission_prob:
             state_probs.append(dict(enumerate(state_prob)))
         return state_probs
-
-    @property
-    def transmat(self):
-        try:
-
-            return self._model.dense_transition_matrix()
-        except AttributeError as e:
-            print(f"Model not initialized with fit, exception was raised: {e}")
-
-    @property
-    def startprob(self):
-        try:
-            return self._model.start
-        except AttributeError as e:
-            print(f"Model not initialized with fit, exception was raised: {e}")
-
-    @property
-    def emissionprob(self):
-        try:
-            return self.create_emissionprob()
-        except AttributeError as e:
-            print(f"Model not initialized with fit, exception was raised: {e}")
-
-    def create_emissionprob(self):
-        emissionprob = []
-        for state in self._model.states:
-            emissionprob.append(np.array(list(state.distribution.values())))
-        return np.vstack(emissionprob)
-
+"""
